05e116b8713630b28a9a5a087e2037d325d8cd0e
GoldinBaloyi <goldin.baloyi@za.zetes.com>
2025-09-16 06:36:31 +0200

    validations

--- src/LeaveMgmt.Website/Models/AuthModels.cs
+++ src/LeaveMgmt.Website/Models/AuthModels.cs
@@ -4,8 +4,8 @@ namespace LeaveMgmt.Website.Models;
 
 public class LoginBody
 {
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    [Required][EmailAddress] public string Email { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
 }
 
 public sealed class RegisterBody
